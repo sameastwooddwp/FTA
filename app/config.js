7fe89d0424eed5b3a0fdee5643548b9d1ed1121e
Sam <sameastwood.digitaldwp@gmail.com>
2018-09-20 13:56:52 +0100

    sign in

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Personal Independence Payment',
 
   // Default port that prototype runs on
   port: '3000',
